04b641b89148fb19e06451d8fb31951ae0263b26
watchmaker <dariusz.chojnacki@gmail.com>
2015-11-14 22:28:50 +0100

    Adding spring-boot as start project.

---
+++ src/main/java/org/varsum/game_of_life/GameOfLifeApplication.java
@@ -0,0 +1,15 @@
+package org.varsum.game_of_life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author The Watchmaker
+ */
+@SpringBootApplication
+public class GameOfLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameOfLifeApplication.class, args);
+    }
+}
---
+++ src/test/groovy/org/varsum/game_of_life/GameOfLifeApplicationTests.groovy
@@ -0,0 +1,18 @@
+package org.varsum.game_of_life
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+@RunWith(SpringJUnit4ClassRunner)
+@SpringApplicationConfiguration(classes = GameOfLifeApplication)
+@WebAppConfiguration
+class GameOfLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
